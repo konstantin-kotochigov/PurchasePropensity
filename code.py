fae79148213bf161646d87ad432c5060857ecc3f
KK <k.kotochigov@cleverdata.ru>
2019-01-18 21:38:38 +0300

    adde fi

--- code.py
+++ code.py
@@ -1,5 +1,8 @@
 # Imports
 import pandas
+from sklearn.feature_selection import chi2
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import roc_auc_score
 
 # Parameters
 input_dir = "./data"
@@ -48,12 +51,29 @@ nominal_attrs = list(df.select_dtypes(include=['object','category']).columns)
 numeric_attrs = list(df.select_dtypes(include=['float64']).columns)
 numeric_attrs.remove('target')
 
+dummy_df = pandas.get_dummies(data=df[nominal_attrs], drop_first=True)
+dummy_df.columns = ["dummy_"+x for x in dummy_df.columns]
+dummy_attrs = list(dummy_df.columns)
+df = pandas.concat([df, dummy_df], axis=1)
+
+numeric_attrs = numeric_attrs + dummy_attrs
 
 # Cross-correlation
 
 
 # Univariate Feature Importance
+numeric_importance = pandas.DataFrame({"attr":numeric_attrs})
+nominal_importnace = pandas.DataFrame({"attr":nominal_attrs})
 
+lr =  LogisticRegression()
+
+rocauc_importance = []
+for attr in numeric_attrs:
+    notnull_rows = df[attr].isna()==False
+    lr.fit(df[attr][notnull_rows].to_frame(), df.target[notnull_rows])
+    y_pred = lr.predict(df[attr][notnull_rows].to_frame())
+    rocauc_importance.append(roc_auc_score(df.target[notnull_rows], y_pred))
+numeric_importance['auc'] = rocauc_importance
 
 # Feature generation
 
