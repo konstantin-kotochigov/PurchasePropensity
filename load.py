a1f190bc2447b8596a8e81909393c1ddced52768
KK <konstantin.kotochigov@gmail.com>
2019-02-08 19:47:55 +0300

    Customer Journey

--- load.py
+++ load.py
@@ -18,15 +18,19 @@ scoring_features = "dmp_crm_attributes.csv"
 
 # Load Features Data
 train_df = spark.read.option("delimiter",";").option("header","true").csv("/".join([train_dir, train_features]))
-train_df = train_df.toDF("crmid","app_ts","source","savings","score","loans_flag","card_flag","card_limit")
+train_df = train_df.toDF("crmid","app_ts","source","tl_account","credit_score","has_loans","active_card","card_limit")
 train_df = train_df.drop("source")
-train_df = train_df.withColumn("target", lit(1))
+train_df = train_df.withColumn("app_ts",to_date(lit("app_ts")))
+train_df = train_df.select("crmid","app_ts","tl_account","credit_score","has_loans","active_card","card_limit")
 
 train_df.createOrReplaceTempView("train_df")
 
 scoring_df = spark.read.option("delimiter",";").option("header","true").csv("/".join([scoring_dir, scoring_features]))
-scoring_df1 = spark.read.option("delimiter",";").option("header","true").csv("/".join([scoring_dir, scoring_f1]))
-scoring_df2 = spark.read.option("delimiter",";").option("header","true").csv("/".join([scoring_dir, scoring_f2]))
+scoring_df = scoring_df.toDF("crmid", "tl_account", "credit_score", "has_loans", "active_card", "card_limit_dummy", "card_limit").drop("card_limit_dummy")
+scoring_df = scoring_df.withColumn("app_ts",to_date(lit("2019-01-25")))
+scoring_df = scoring_df.select("crmid", "app_ts", "tl_account", "credit_score", "has_loans", "active_card", "card_limit")
+# scoring_df1 = spark.read.option("delimiter",";").option("header","true").csv("/".join([scoring_dir, scoring_f1]))
+# scoring_df2 = spark.read.option("delimiter",";").option("header","true").csv("/".join([scoring_dir, scoring_f2]))
 
 
 
@@ -40,17 +44,17 @@ cp_path = '/data/6287566b-8263-497b-8a4e-fbdb2680df1e/.dmpkit/profiles/master/cd
 cj = spark.read.format("com.databricks.spark.avro").load(cj_path)
 time_from = int(datetime.datetime(2018, 12, 30).timestamp()) * 1000
 time_to = int(datetime.datetime(2018, 12, 31).timestamp()) * 1000
-cj = cj_part.filter('ts > {} and ts < {}'.format(time_from, time_to))
-cj.createOrReplaceTempView('cj_part')
+cj = cj.filter('ts > {} and ts < {}'.format(time_from, time_to))
+cj.createOrReplaceTempView('cj')
 
 # Load Customer Profile
 cp_part = spark.read.format("com.databricks.spark.avro").load(cp_path)
 time_from = int(datetime.datetime(2018, 12, 30).timestamp()) * 1000
 time_to = int(datetime.datetime(2018, 12, 31).timestamp()) * 1000
 cp = cp_part.filter('ts > {} and ts < {}'.format(time_from, time_to))
-cp.createOrReplaceTempView('cj_part')
+cp.createOrReplaceTempView('cj')
 
 # Convert to Python dataframes
-train_py = pandas.DataFrame(train_df.collect(), columns=train_df.columns)
-scoring_py = pandas.DataFrame(scoring_df.collect(), columns=scoring_df.columns)
+train_py = pandas.DataFrame(train_df.collect(), columns=train_df.columns, dtype='float64')
+scoring_py = pandas.DataFrame(scoring_df.collect(), columns=scoring_df.columns, dtype='float64')
 
