43215d86953e88dcdda348586ffaed838f381b45
KK <konstantin.kotochigov@gmail.com>
2019-02-11 23:50:35 +0300

    Customer Journey

--- code.py
+++ code.py
@@ -43,7 +43,7 @@ df['loans'] = pandas.cut(df.loans, [-1,0,1,1000], labels=['loans=0','loans=1','l
 
 # Classify column types
 nominal_attrs = list(df.select_dtypes(include=['object','category']).columns)
-numeric_attrs = list(df.select_dtypes(include=['float64']).columns)
+numeric_attrs = list(df.select_dtypes(include=['uint8','float64']).columns)
 numeric_attrs.remove('target')
 
 dummy_df = pandas.get_dummies(data=df[nominal_attrs], drop_first=True)
@@ -76,9 +76,9 @@ lr =  LogisticRegression(solver='lbfgs')
 rocauc_importance = []
 for attr in numeric_attrs:
     print("Processing ",attr)
-    lr.fit(df[attr].to_frame(), df.target)
+    lr.fit(X[attr].to_frame(), y)
     y_pred = lr.predict_proba(df[attr].to_frame())[:,1]
-    rocauc_importance.append(roc_auc_score(df.target, y_pred))
+    rocauc_importance.append(roc_auc_score(y, y_pred))
 
 numeric_importance['auc'] = rocauc_importance
 
@@ -86,20 +86,23 @@ numeric_importance['auc'] = rocauc_importance
 
 # GridSearchCV
 
+
 X = df[numeric_attrs]
 y = df.target
 
 # ros = RandomOverSampler(random_state=0)
 # X_resampled, y_resampled = ros.fit_resample(X, y)
 
-X = pandas.concat([X, X[y==1]], axis=0)
-y = pandas.concat([y, y[y==1]], axis=0)
+# X = pandas.concat([X, X[y==1]], axis=0)
+# y = pandas.concat([y, y[y==1]], axis=0)
 
-lrParamGrid={"penalty":("l1","l2"), "C":(0.1,0.5,1.0)}
+lr = LogisticRegression(solver='lbfgs', penalty='l2', C=0.1)
+lrParamGrid={"penalty":["l2"], "C":(0.1,0.5,1.0)}
 lrCV = GridSearchCV(lr, param_grid=lrParamGrid, scoring='roc_auc', cv=5, verbose=0, n_jobs=-1)
-lrCV.fit(X,y)
+lrCV.fit(X[numeric_attrs1],y)
 cv_results = lrCV.cv_results_
 cv_table = pandas.DataFrame({"param":cv_results['params'], "error":cv_results['mean_test_score']}).sort_values(by="error", ascending=False)
+cv_table
 # cv_table.to_csv("re/gridsearch/coordinates_randomforest.csv", index=False)
 
 lr = LogisticRegression(solver='lbfgs', penalty='l2', C=0.1, njobs=-1)
--- load.py
+++ load.py
@@ -2,40 +2,35 @@
 
 # Output: train_df, score_df, cj, cp
 
-import datetime
-import pandas
-from pyspark.sql.types import ArrayType, StringType
-from pyspark.sql.functions import lit
-
-# Input parameters
-train_dir = "/data/6287566b-8263-497b-8a4e-fbdb2680df1e/Machine Learning_People who gets loan succesfully recently"
-train_features = "DUMMY ID & Attrıbutes_dmpuploadfile.csv"
-train_target = "ids.csv"
-scoring_dir = "/data/6287566b-8263-497b-8a4e-fbdb2680df1e/Machine learning"
-scoring_f1 = "Onlystep4completion.csv"
-scoring_f2 = "allstepscompletion.csv"
-scoring_features = "dmp_crm_attributes.csv"
+import datetime# Input parameters
+train_dir = "/analytics/"
+train_features = "train.csv"
+# train_target = "ids.csv"
+scoring_dir = "/analytics/"
+# scoring_f1 = "Onlystep4completion.csv"
+# scoring_f2 = "allstepscompletion.csv"
+scoring_features = "scoring.csv"
 
 # Load Features Data
 train_df = spark.read.option("delimiter",";").option("header","true").csv("/".join([train_dir, train_features]))
-train_df = train_df.toDF("crmid","app_ts","source","tl_account","credit_score","has_loans","active_card","card_limit")
-train_df = train_df.drop("source")
-train_df = train_df.withColumn("app_ts",to_date(lit("app_ts")))
-train_df = train_df.select("crmid","app_ts","tl_account","credit_score","has_loans","active_card","card_limit")
+train_df = train_df.toDF("crmid","id","app_ts","source","tl_account","credit_score","has_savings_account","num_loans","active_card","card_limit","usable_card_limit")
+train_df = train_df.drop("id","source","has_savings_account")
+train_df = train_df.withColumn("app_ts",to_date(train_df["app_ts"]))
+train_df = train_df.select("crmid","app_ts","tl_account","credit_score","num_loans","active_card","card_limit","usable_card_limit")
 
 train_df.createOrReplaceTempView("train_df")
 
 scoring_df = spark.read.option("delimiter",";").option("header","true").csv("/".join([scoring_dir, scoring_features]))
-scoring_df = scoring_df.toDF("crmid", "tl_account", "credit_score", "has_loans", "active_card", "card_limit_dummy", "card_limit").drop("card_limit_dummy")
+scoring_df = scoring_df.toDF("crmid", "tl_account", "credit_score", "num_loans", "active_card", "card_limit","usable_card_limit")
 scoring_df = scoring_df.withColumn("app_ts",to_date(lit("2019-01-25")))
-scoring_df = scoring_df.select("crmid", "app_ts", "tl_account", "credit_score", "has_loans", "active_card", "card_limit")
+scoring_df = scoring_df.select("crmid", "app_ts", "tl_account", "credit_score", "num_loans", "active_card", "card_limit", "usable_card_limit")
 # scoring_df1 = spark.read.option("delimiter",";").option("header","true").csv("/".join([scoring_dir, scoring_f1]))
 # scoring_df2 = spark.read.option("delimiter",";").option("header","true").csv("/".join([scoring_dir, scoring_f2]))
 
 
 
 # Load Target Data
-target = spark.read.option("delimiter",";").option("header","true").csv("/".join([train_dir, train_target]))
+# target = spark.read.option("delimiter",";").option("header","true").csv("/".join([train_dir, train_target]))
 
 cj_path = '/data/6287566b-8263-497b-8a4e-fbdb2680df1e/.dmpkit/customer-journey/master/cdm'
 cp_path = '/data/6287566b-8263-497b-8a4e-fbdb2680df1e/.dmpkit/profiles/master/cdm/'
@@ -57,4 +52,9 @@ cp.createOrReplaceTempView('cj')
 # Convert to Python dataframes
 train_py = pandas.DataFrame(train_df.collect(), columns=train_df.columns, dtype='float64')
 scoring_py = pandas.DataFrame(scoring_df.collect(), columns=scoring_df.columns, dtype='float64')
+
+
+import pandas
+from pyspark.sql.types import ArrayType, StringType
+from pyspark.sql.functions import lit, to_date
 
